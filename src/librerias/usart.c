b24272261a952c2f5effb1322dbdbc1a137e1046
Greskad74 <zayaman74@gmail.com>
2025-04-29 13:18:53 -0600

    test1

--- src/librerias/usart.c
+++ src/librerias/usart.c
@@ -10,7 +10,8 @@ void usart1_sendbyte(uint16_t data){
 
 void usart1_sendstring(char s[]){
 	int i = 0;
-	while(i < 64){
+	int t = sizeof(s);
+	while(i < t){
 		if (s[i] == '\0') break;
 		usart1_sendbyte(s[i++]);
 	}
@@ -45,3 +46,31 @@ void usart_polling(){
 }
 
 
+void usart1_w_interrupt(){
+	
+	
+	RCC->APB2ENR |= (1 << 14);
+	RCC->APB2ENR |= (1 << 2);  // Habilita el reloj para GPIOA
+	//usart1_tx PA9
+	//usart1_rx PA10
+	GPIOA->ODR |= (1 << 10);
+
+	GPIOA->CRH &= ~((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4));  // LIMPIA PA 9
+	GPIOA->CRH |= (1 << 7) | (1 << 5) | (1 << 4);   // PA9 SALIDA A 50 MHZ PUSHPULL
+	GPIOA->CRH &= ~((1 << 11) | (1 << 10) | (1 << 9) | (1 << 8));  // Reseteo a Pa10, Entrada 
+	GPIOA->CRH |= (1 << 10); // Opendrai 
+
+	USART1->CR1 |= (1 << 2 | 1 << 3 | 1 << 13); //enable rx,tx and USART
+	/* Example Baud rate 9600
+	BRR = fpclk/bayd rate = 72,000,000/9600 = 7,500 */
+	USART1->BRR = 0x341; //set baudrate
+	USART1->CR1 |= (1 << 7);	//TXE enable interrupt
+	NVIC_EnableIRQ(USART1_IRQn);//enable global interrupts
+}
+
+void USART1_IRQHandler(){
+	if(USART1->CR1 & USART1->SR & USART_SR_TXE) {
+        usart1_sendstring("Hey Nana");
+		
+	}
+}
