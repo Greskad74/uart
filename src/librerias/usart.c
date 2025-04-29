7a4b5fa5b0237bb82faff74e60bfaf89e02610ae
Greskad74 <zayaman74@gmail.com>
2025-04-29 10:53:51 -0600

    8mhz

--- src/librerias/usart.c
+++ src/librerias/usart.c
@@ -18,19 +18,27 @@ void usart1_sendstring(char s[]){
 
 
 void usart_polling(){
-	RCC->APB2ENR |= 0xFC | (1 << 14);
+	RCC->APB2ENR |= (1 << 14);
+	RCC->APB2ENR |= (1 << 2);  // Habilita el reloj para GPIOA
 	//usart1_tx PA9
 	//usart1_rx PA10
 	GPIOA->ODR |= (1 << 10);
-	GPIOA->CRH = 0x444448B4;
-	USART1->CR1 = 0x200C;
-	USART1->BRR = 7500;
+
+	GPIOA->CRH &= ~((1 << 7) | (1 << 6) | (1 << 5) | (1 << 4));  // LIMPIA PA 9
+	GPIOA->CRH |= (1 << 7) | (1 << 5) | (1 << 4);   // PA9 SALIDA A 50 MHZ PUSHPULL
+	GPIOA->CRH &= ~((1 << 11) | (1 << 10) | (1 << 9) | (1 << 8));  // Reseteo a Pa10, Entrada 
+	GPIOA->CRH |= (1 << 10); // Opendrai 
+
+
+
+	USART1->CR1 |= (1 << 2) | (1 << 3) | (1 << 13);  // Activa RX, TX y USART
+	USART1->BRR = 0x341;
 	while(1){
 		usart1_sendbyte('H');
-		usart1_sendbyte('o');
-		usart1_sendbyte('l');
-		usart1_sendbyte('a');
-		usart1_sendstring("USART");
+		usart1_sendbyte('E');
+		usart1_sendbyte('Y');
+		usart1_sendbyte(' ');
+		usart1_sendstring("NANA");
 		usart1_sendbyte('\n');
 		usart1_sendbyte('\r');
 	}
